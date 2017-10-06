1a6dd6465edc58b8280b8ed8aad16bbb9fcd9ab8
awarn <anton@rebelandbird.com>
2017-10-07 01:37:36 +0200

    Bootstrap style the menu

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -8,11 +8,22 @@ export default class Navigation extends React.Component {
 
 	render() {
 		return (
-			<nav>
-				<ul>
-					<li><a href="/">Home</a></li>
-					<li><a href="/work">Work</a></li>
-				</ul>
+			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+				<a className="navbar-brand" href="#">Cashier</a>
+				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+
+				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul className="navbar-nav mr-auto">
+						<li className="nav-item">
+							<a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="/work">Work</a>
+						</li>
+					</ul>
+				</div>
 			</nav>
 		)
 	}
